89471299d10ca97343cf89251a1c32d43ae29787
dongkyu92 <dongkyu92@naver.com>
2021-03-21 18:10:15 +0900

    lombok 의존성 코드 추가, 주석 추가

--- src/test/java/com/dongkyu/board/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/dongkyu/board/springboot/web/dto/HelloResponseDtoTest.java
@@ -18,5 +18,12 @@ public class HelloResponseDtoTest {
         //then
         assertThat(dto.getName()).isEqualTo(name);
         assertThat(dto.getAmount()).isEqualTo(amout);
+        // assertj라는 테스트 검증 라이브러리의 검증 메소드임.
+        // 검증하고 싶은 대상을 메소드 인자로 받음.
+        // 메소드 체이닝이 지원되어 isEqualTo와 같이 메소드를 이어서 사용할 수 있음.
+
+        //Junit에서 제공하는 assertThat이 아닌 assertj의 assertThat을 사용하는 이유.
+        // 추가적인 라이브러리가 필요하지 않음.
+        // 자동완성이 좀 더 확실하게 지원됨.
     }
 }
