341b1ee633c4f81297b26c233cb90ad6c547b819
dongkyu92 <dongkyu92@naver.com>
2021-03-27 21:49:40 +0900

    JpaRepository, 테스트 코드 생성

---
+++ src/test/java/com/dongkyu/board/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,15 @@
+package com.dongkyu.board.springboot.domain.posts;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+}
